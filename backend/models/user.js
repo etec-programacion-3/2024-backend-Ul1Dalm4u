8df38b959b8e8bfdb092ab1699e6dae62fdf94f8
u.dalmau@alumno.etec.um.edu.ar <u.dalmau@alumno.etec.um.edu.ar>
2024-11-18 19:15:44 -0300

    login

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcryptjs');
 const sequelize = require('../config/database');
 
 class User extends Model {}
@@ -17,7 +18,7 @@ User.init({
         allowNull: false,
         unique: true,
         validate: {
-            isEmail: true, 
+            isEmail: true,
         },
     },
     password: {
@@ -28,5 +29,16 @@ User.init({
     sequelize,
     modelName: 'User',
 });
+
+// Antes de guardar el usuario, cifrar la contraseña
+User.beforeCreate(async (user) => {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+});
+
+// Método para comparar la contraseña durante el login
+User.prototype.comparePassword = function(password) {
+    return bcrypt.compare(password, this.password);
+};
 
 module.exports = User;
